f40894caf0196969993e54f2068f8de34ef409fc
hi <astitmnak1@gmial.com>
2024-02-03 14:22:45 +0330

    add onChangeHandler to searchForm component

--- src/components/weather/SearchForm.tsx
+++ src/components/weather/SearchForm.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 
 function SearchForm(props) {
+    let CityName ="";
+    const onchangeHandler = (e)=>{
+
+        CityName = e.target.value;
+
+    }
+    const onSubmitHandler = () => {
+        console.log(CityName);
+    }
     return (
        <div className={" w-full flex justify-center mb-8 mt-28"}>
            <form>
-               <input className={" border px-8 py-4 rounded mr-3 "} type={"text"} />
-               <input className={"bg-orange-300 px-8 py-4 rounded text-white "} type={"submit"} value={"Search"}/>
+               <input className={" border px-8 py-4 rounded mr-3 "} type={"text"}  onChange={onchangeHandler}/>
+               <input className={"bg-orange-300 px-8 py-4 rounded text-white "} type={"submit"} value={"Search"} onSubmit={onSubmitHandler}/>
            </form>
        </div>
     );
