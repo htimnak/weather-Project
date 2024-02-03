c44a2c11af733bfdd9443d7b98107b69039a1a18
hi <astitmnak1@gmial.com>
2024-02-03 14:57:21 +0330

    add state to searchForm component

--- src/components/weather/SearchForm.tsx
+++ src/components/weather/SearchForm.tsx
@@ -1,20 +1,25 @@
-import React from 'react';
+import {useState} from "react";
 
-function SearchForm(props) {
-    let CityName ="";
-    const onchangeHandler = (e)=>{
 
-        CityName = e.target.value;
+function SearchForm() {
+    const [cityNameState,setCityNameState] = useState("");
+
+    const changeCityNameHandler = (e)=>{
+        setCityNameState(e.target.value);
+
 
     }
-    const onSubmitHandler = () => {
-        console.log(CityName);
+    const submitHandler = (e) => {
+        e.preventDefault();
+
+        console.log(cityNameState);
+        setCityNameState("");
     }
     return (
-       <div className={" w-full flex justify-center mb-8 mt-28"}>
+       <div className={" w-full flex justify-center mb-8 mt-28"} onSubmit={submitHandler }>
            <form>
-               <input className={" border px-8 py-4 rounded mr-3 "} type={"text"}  onChange={onchangeHandler}/>
-               <input className={"bg-orange-300 px-8 py-4 rounded text-white "} type={"submit"} value={"Search"} onSubmit={onSubmitHandler}/>
+               <input className={" border px-8 py-4 rounded mr-3 "} type={"text"} value={cityNameState}  onChange={changeCityNameHandler}/>
+               <input className={"bg-orange-300 px-8 py-4 rounded text-white "} type={"submit"} value={"Search"}/>
            </form>
        </div>
     );
